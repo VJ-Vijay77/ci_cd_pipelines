aa265ec73a1d1225d7fad0a6e59029bbbc203a8e
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-08-10 23:06:44 +0530

    added functions

--- main.go
+++ main.go
@@ -5,6 +5,30 @@ import "fmt"
 func main() {
 	output := Greet()
 	fmt.Println(output)
+	AddingTwoNumbers := Add(4,5)
+	fmt.Println(AddingTwoNumbers)
+
+	SubstractingTwoNumbers := Substract(5,2)
+	fmt.Println(SubstractingTwoNumbers)
+
+	AddTailToString := Tail("sample")
+	fmt.Println(AddTailToString)
+}
+
+func Tail(s string) string {
+	t := "-TailAdded"
+	new := s + t
+	return new
+}
+
+
+func Add(a int,b int) int{
+	sum := a + b
+	return sum
+}
+func Substract(a int,b int) int{
+	sum := a - b
+	return sum
 }
 
 func Greet() string {
--- main_test.go
+++ main_test.go
@@ -6,6 +6,25 @@ func TestMain(t *testing.T) {
  output := Greet()
 
  if output != "Hello test" {
-	t.Errorf("Wanted -'Hello test' \n got - %s",output)
+	t.Errorf("\n\nWanted -'Hello test' \n got - %s\n\n",output)
  }
+
+ output2 := Add(5,6)
+ if output2 != 11 {
+	t.Errorf("\n\nWanted -11 \ngot - %d\n\n",output2)
+ }
+ 
+ 
+ output3 := Substract(10,6)
+ if output3 != 4 {
+	t.Errorf("\n\nWanted -4 \ngot - %d\n\n",output3)
+ }
+ 
+ output4 := Tail("test")
+ expected := "test-TailAdded"
+ if output4 != expected {
+	t.Errorf("\n\nWanted - %s \nGot - %s\n\n",expected,output4)
+ }
+
+
 }
